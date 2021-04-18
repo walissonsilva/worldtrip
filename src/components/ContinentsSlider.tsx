5888c79ff6c015b583455cc680eb3611918e14bd
Walisson Silva <walissonsilva10@gmail.com>
2021-04-18 16:08:37 -0300

    feat: add Swiper slider lib to ContinentsSlider component

--- src/components/ContinentsSlider.tsx
+++ src/components/ContinentsSlider.tsx
@@ -1,5 +1,31 @@
+import { Box, Image } from '@chakra-ui/react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
 export function ContinentsSlider() {
   return (
-    <h1>Slider</h1>
+    <Box
+      maxWidth={1160}
+      mx="auto"
+      mt="52px"
+    >
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={1}
+        onSlideChange={() => console.log('slide change')}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        <SwiperSlide>
+          <Image src="/images/continents/europe.png" alt="Europe Slider" />
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <Image src="/images/continents/america.jpg" alt="America Slider" />
+        </SwiperSlide>
+
+        <SwiperSlide>
+          Slide 3
+        </SwiperSlide>
+      </Swiper>
+    </Box>
   )
 }
